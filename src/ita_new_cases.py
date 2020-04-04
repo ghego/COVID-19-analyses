94cb8f0f5ae90982bc6dda18e2800c4874d36500
Francesco Mosconi <git@mosconi.me>
2020-04-04 10:13:04 -0700

    update data

--- src/ita_new_cases.py
+++ src/ita_new_cases.py
@@ -5,14 +5,22 @@ import matplotlib.dates as mdates
 
 dfita = pd.read_csv('../data/pcm-dpc_COVID-19/dati-regioni/dpc-covid19-ita-regioni.csv')
 dfita['data'] = pd.to_datetime(dfita['data'])
-dfitapvt = dfita.pivot(columns='denominazione_regione', index='data', values='totale_casi')
-ts = dfitapvt.sum(axis=1).diff()
+
+pvt = dfita.drop(['stato', 'codice_regione', 
+                  'lat', 'long', 
+                  'note_it', 'note_en'], axis=1).groupby(['data']).sum()
+
+tss = pvt.diff(axis=0)
+
+cols = ['totale_casi', 'dimessi_guariti', 'deceduti']
 
 fig, ax = plt.subplots(figsize=(10,6))
-ax.bar(ts.index, ts.values)
+for c in cols:
+    ax.bar(tss.index, tss[c])
 ax.xaxis.set_major_locator(mdates.WeekdayLocator())
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
-ax.set_title('Italy: New cases')
+ax.set_title('Italy')
 ax.set_ylabel('Count')
 ax.set_xlabel('Date')
+ax.legend(cols)
 fig.savefig("../images/italy_new_cases.png")
